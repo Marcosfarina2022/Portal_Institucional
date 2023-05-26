440480ffa8196df70cba9dc433737062ec1e16a8
nicolas-vega-tdf <nicolasvega@tdf.edu.ar>
2023-05-25 21:25:36 -0300

    crecion de componentes y navbar

---
+++ tp-final/src/Components/Header/Header.jsx
@@ -0,0 +1,11 @@
+import NavBar from '../NavBar/NavBar';
+import './Header.css'; 
+
+const Header = () => {
+return(<div className='header'>
+    <div className='titulo'><h1>Portal Institucional</h1></div>
+    <NavBar/>
+</div>)
+}
+
+export default Header
