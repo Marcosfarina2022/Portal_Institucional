a44fd1b08d34ead676d8a6109e504367fade6375
SIMONETTIGUIDO1993 <guidosimonetti@tdf.edu.ar>
2023-06-01 13:11:40 -0300

    Simplificando Footer

--- tp-final/src/Components/Footer/Footer.jsx
+++ tp-final/src/Components/Footer/Footer.jsx
@@ -5,37 +5,25 @@ import { FaFacebook, FaInstagram, FaYoutube } from "react-icons/fa";
 function Footer() {
     return (
         <footer className="footer">
-            <div class="td-footer-wrapper td-container-wrap ">
 
-                <div class="td-footer-bottom-full">
-                    <div class="td-container">
-                        <div class="td-pb-row">
-                            <div className='div-logo'>
-                                <img className='logo' src={logoCLA} alt="Logo" />
-                            </div>
-                            <div class="td-pb-span5"><aside class="footer-text-wrap"><div class="block-title"></div>Club familiar de Rugby y Hockey más austral del mundo.<div class="footer-email-wrap">Contáctanos: <a href="mailto:clasaguilas@gmail.com">clasaguilas@gmail.com</a></div></aside></div>
-                            <div class="td-pb-span4">
-                                <aside class="footer-social-wrap td-social-style-2">
-                                    <div class="block-title"><span>SEGUINOS</span></div>
-                                    <span class="td-social-icon-wrap">
-                                        <a target="_blank" href="https://www.facebook.com/clublasaguilasushuaia/?locale=es_LA" title="Facebook">
-                                            <FaFacebook />
-                                        </a>
-                                    </span>
-                                    <span class="td-social-icon-wrap">
-                                        <a target="_blank" href="https://www.instagram.com/clublasaguilas/" title="Instagram">
-                                            <FaInstagram />
-                                        </a>
-                                    </span>
-                                    <span class="td-social-icon-wrap">
-                                        <a target="_blank" href="https://www.youtube.com/@clublasaguilas-analisisdev8566" title="Youtube">
-                                            <FaYoutube />
-                                        </a>
-                                    </span></aside></div>
-                        </div>
-                    </div>
-                </div>
+            <div className='div-logo'>
+                <img className='logo' src={logoCLA} alt="Logo" />
             </div>
+            <div className='div-contacto'>
+                <div>Club familiar de Rugby y Hockey más austral del mundo.</div>
+                <div class="footer-email">Contáctanos: <a href="mailto:clasaguilas@gmail.com">clasaguilas@gmail.com</a></div>
+            </div>
+            <div className='div-redes'>
+                <span>SEGUINOS</span>
+                <a target="_blank" href="https://www.facebook.com/clublasaguilasushuaia/?locale=es_LA" title="Facebook">
+                    <FaFacebook /></a>
+                <a target="_blank" href="https://www.instagram.com/clublasaguilas/" title="Instagram">
+                    <FaInstagram /></a>
+                <a target="_blank" href="https://www.youtube.com/@clublasaguilas-analisisdev8566" title="Youtube">
+                    <FaYoutube /></a>
+            </div>
+
+
             <p>&copy; 2023 - MentesDeMentes</p>
         </footer>
     );
