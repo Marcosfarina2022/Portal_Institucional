042a3d6df23fe0e638377731ee5a8a1166bc5ab4
SIMONETTIGUIDO1993 <guidosimonetti@tdf.edu.ar>
2023-06-02 22:51:21 -0300

    agregue y cambie logo

--- tp-final/src/Components/Footer/Footer.jsx
+++ tp-final/src/Components/Footer/Footer.jsx
@@ -1,27 +1,27 @@
 import './Footer.css'
-import logoCLA from "../../Imgenes/LogoCLA2.png";
+import logoClaBlanco from "../../Imgenes/logoClaBlanco.png";
 import { FaFacebook, FaInstagram, FaYoutube } from "react-icons/fa";
 
 function Footer() {
     return (
         <footer className="footer">
 
-            <div class="flex-container">
+            <div className="flex-container">
 
-                <div class='div-logo'>
-                    <img className='logo' src={logoCLA} alt="Logo" />
+                <div className='div-logo'>
+                    <img className='logo' src={logoClaBlanco} alt="Logo" />
                 </div>
 
-                <div class='div-contacto'>
+                <div className='div-contacto'>
                     <div>Club familiar de Rugby y Hockey más austral del mundo.</div>
 
-                    <div class="footer-email">Contáctanos: <a href="mailto:clasaguilas@gmail.com">clasaguilas@gmail.com</a></div>
+                    <div className="footer-email">Contáctanos: <a href="mailto:clasaguilas@gmail.com">clasaguilas@gmail.com</a></div>
                 </div>
 
-                <div class='div-redes'>
+                <div className='div-redes'>
                     <div>SEGUINOS</div>
 
-                    <div class="div-icons">
+                    <div className="div-icons">
                         <div><a target="_blank" href="https://www.facebook.com/clublasaguilasushuaia/?locale=es_LA" title="Facebook">
                             <FaFacebook /></a></div>
                         <div>                        <a target="_blank" href="https://www.instagram.com/clublasaguilas/" title="Instagram">
@@ -34,8 +34,9 @@ function Footer() {
                     </div>
                 </div>
             </div>
-
+            <div className="div-copyright">
             <p>&copy; 2023 - MentesDeMentes</p>
+            </div>
         </footer>
     );
 }
