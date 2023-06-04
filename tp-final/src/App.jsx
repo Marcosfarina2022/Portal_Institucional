118702f69b63b33204d738f4352d1101faeaa45f
Kevin <kevinsantander@tdf.edu.ar>
2023-06-04 16:42:15 -0300

    se creo el componente FormLogin y se agregaron dep

--- tp-final/src/App.jsx
+++ tp-final/src/App.jsx
@@ -1,12 +1,16 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
+import { BrowserRouter, Route, Routes} from 'react-router-dom';
 import Home from './Components/Home/Home';
+import FormLogin from './Components/FormLogin/FormLogin';
 
 function App() {
   return (
-    <div className="App">
-      <Home></Home>
-    </div>
+    <BrowserRouter>
+      <div className="App">
+       <Home />
+      </div>
+    </BrowserRouter>
   );
 }
 
