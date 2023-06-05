5cdca56d3e3218f562c5bd4b8452ca171179039c
Marcosfarina2022 <marcosfarina@tdf.edu.ar>
2023-06-04 21:36:06 -0300

    creación de Cards en la sección noticias

--- tp-final/src/Components/Home/Home.jsx
+++ tp-final/src/Components/Home/Home.jsx
@@ -2,6 +2,7 @@
 import Header from "../Header/Header";
 import Footer from "../Footer/Footer";
 import Content from "../Content/Content";
+import Carousel from "../Carousel/Carousel";
 import './Home.css'
 
 
@@ -9,7 +10,8 @@ import './Home.css'
 const Home = () =>{
  return ( 
     <div className="home">
-    {/* <div><Header></Header></div> lo comento porque se duplica al navegar */}
+    <Carousel/>
+    {/*<div><Header></Header></div> {/*lo comento porque se duplica al navegar */}
     <div><Content></Content></div>
     <div><Footer></Footer></div>
 
