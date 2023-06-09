fa6ce68e49701f225885dbbd752d6ad54ea2bf1e
Marcosfarina2022 <marcosfarina@tdf.edu.ar>
2023-06-08 23:14:44 -0300

    se continuo con el trabajo de cards, estilos y se creo el componente sponsors y se modifico la navbar

--- tp-final/src/Components/Content/Content.jsx
+++ tp-final/src/Components/Content/Content.jsx
@@ -1,79 +1,15 @@
 import React from 'react';
-import Card from 'react-bootstrap/Card';
-import Button from 'react-bootstrap/Button';
-import Image1 from '../../Imgenes/imgNoticias/1.svg';
-import Image2 from '../../Imgenes/imgNoticias/2.svg';
-import Image3 from '../../Imgenes/imgNoticias/3.svg';
-import sidebar1 from '../../Imgenes/imgNoticias/sidebar1.svg';
 import './Content.css';
-import { Col, Row, Image, Nav } from 'react-bootstrap';
+import Cards from "../Cards/Cards";
+import Sponsors from "../Sponsors/Sponsors";
 
 const Content = () => {
-  return (<> 
-    <Row className='contenedor'>
-      <Col md={8}>
-        <Row>
-          <Col md={6}>
-            {/*Card numero 1*/}
-            <Card style={{ width: '18rem' }}>
-              <Card.Body>
-              <Card.Img variant="top" src={Image1}/>
-                <Card.Title>Águilas Jr</Card.Title>
-                <Card.Text>Nuestra escuela de rugby para niños ofrece una experiencia deportiva divertida y educativa.</Card.Text>
-                <Button variant="primary">Leer más...</Button>
-              </Card.Body>
-            </Card>
-          </Col>
-          <Col md={6}>
-           {/*Card numero 2*/}
-            <Card>
-              <Card.Body>
-              <Card.Img variant="top" src={Image2}/>
-                <Card.Title>Equipo de Hockey Femenino</Card.Title>
-                <Card.Text>El equipo de hockey femenino del club ha tenido una destacada participación en los últimos torneos. Han logrado clasificar a la fase final y se encuentran en la lucha por el campeonato. ¡Apoya a nuestras jugadoras y disfruta del emocionante hockey en cada partido!.</Card.Text>
-                <Button variant="primary">Leer más...</Button>
-              </Card.Body>
-            </Card>
-          </Col>
-        </Row>
-        <Row>
-          <Col md={6}>
-            {/*Card numero 3*/}
-            <Card>
-              <Card.Body>
-              <Card.Img variant="top" src={Image3} className="img-fluid"/>
-                <Card.Title>Noticia 3</Card.Title>
-                <Card.Text>Contenido de la noticia 3.</Card.Text>
-                <Button variant="primary">Leer más...</Button>
-              </Card.Body>
-            </Card>
-          </Col>
-          <Col md={6}>
-            {/*Card numero 3*/}
-            <Card>
-              <Card.Body>
-              <Card.Img variant="top" src={Image3}/>
-                <Card.Title>Noticia 4</Card.Title>
-                <Card.Text>Contenido de la noticia 4.</Card.Text>
-                <Button variant="primary">Leer más...</Button>
-              </Card.Body>
-            </Card>
-          </Col>
-        </Row>
-      </Col>
-      <Col md={4}>
-        {/*Sidebar*/}
-        <Card>
-          <Card.Body>
-            <Card.Title>Sidebar</Card.Title>
-            <Card.Img variant="top" src={sidebar1}/>
-            <Card.Text>Contenido del sidebar.</Card.Text>
-            <Button variant="primary">Leer más...</Button>
-          </Card.Body>
-        </Card>
-      </Col>
-    </Row>
-  </>)
+  return (
+    <div>
+    <Cards></Cards>
+    <Sponsors></Sponsors>
+    </div>
+  );
 }
 
 
