f46aaa70da6198096a8172fdb6995be3e46296a4
Melina Figueroa <melinafigueroa@tdf.edu.ar>
2023-06-19 22:04:29 -0300

    se agregan cambios a la seccion sponsors

--- tp-final/src/Components/Sponsors/Sponsors.jsx
+++ tp-final/src/Components/Sponsors/Sponsors.jsx
@@ -1,64 +1,70 @@
-import './sponsors.css';
 import React from 'react';
 import { Link } from 'react-router-dom';
 import logoMacro from '../../Imgenes/imagenesSponsors/logoBancoMacro.svg';
 import logoTanteSara from '../../Imgenes/imagenesSponsors/logoTanteSara.svg';
 import logoPopper from '../../Imgenes/imagenesSponsors/logoPopperSports.svg';
 import logoCerveza from '../../Imgenes/imagenesSponsors/logoCervezaImperial.svg';
+import './sponsors.css';
 
 const sponsorsData = [
-    {
-      id: 1,
-      name: 'Banco Macro',
-      image: logoMacro,
-      alt: ' Banco Macro',
-      link: 'https://www.macro.com.ar/',
-    },
-    {
-      id: 2,
-      name: 'Tante Sara',
-      image: logoTanteSara,
-      alt: 'Tante Sara',
-      link: 'https://tantesaraonline.com/',
-    },
-    {
-      id: 3,
-      name: 'Popper Sports',
-      image: logoPopper,
-      alt: ' Popper Sports',
-      link: 'https://paseodelfuego.com.ar/',
-    },
-    {
-      id: 3,
-      name: 'Cerveza Imperial',
-      image: logoCerveza,
-      alt: ' Cerveza Quilmes',
-      link: 'https://cervezaimperial.com.ar/',
-    }
-  ];
-  
-  function Sponsors() {
-    return (
-      <div className="sponsors-container">
-        <h2 className='tituloPat'>Nuestros Patrocinadores</h2>
-        <div className="sponsors-grid">
-          {sponsorsData.map((sponsor) => (
-            <Link 
-                key={sponsor.id} 
-                to={sponsor.link}
-                target="_blank"
-                rel="noopener noreferrer"
-                >
-              <img
-                className="sponsor-thumbnail"
-                src={sponsor.image}
-                alt={sponsor.patrocinadores}
-              />
-            </Link>
-          ))}
-        </div>
-      </div>
-    );
+  {
+    id: 1,
+    name: 'Banco Macro',
+    image: logoMacro,
+    alt: 'Banco Macro',
+    link: 'https://www.macro.com.ar/',
+    description: 'Un banco comprometido con el desarrollo y crecimiento económico.',
+  },
+  {
+    id: 2,
+    name: 'Tante Sara',
+    image: logoTanteSara,
+    alt: 'Tante Sara',
+    link: 'https://tantesaraonline.com/',
+    description: 'Un restaurante familiar con deliciosos platos caseros.',
+  },
+  {
+    id: 3,
+    name: 'Popper Sports',
+    image: logoPopper,
+    alt: 'Popper Sports',
+    link: 'https://paseodelfuego.com.ar/',
+    description: 'Una tienda especializada en artículos deportivos de calidad.',
+  },
+  {
+    id: 4,
+    name: 'Cerveza Imperial',
+    image: logoCerveza,
+    alt: 'Cerveza Imperial',
+    link: 'https://cervezaimperial.com.ar/',
+    description: 'Una cervecería artesanal que ofrece variedades únicas.',
   }
-  
-  export default Sponsors;
+];
+
+function Sponsors() {
+  return (
+    <div className="sponsors-container">
+      <div className='tituloSponsors'>Agradecemos a quienes nos acompañan a diario</div>
+      <div className="sponsors-grid">
+        {sponsorsData.map((sponsor) => (
+          <Link
+            key={sponsor.id}
+            to={sponsor.link}
+            target="_blank"
+            rel="noopener noreferrer"
+            className="sponsors-item"
+          >
+            <img
+              src={sponsor.image}
+              alt={sponsor.alt}
+              className="sponsors-image"
+            />
+            <div className="sponsors-description">{sponsor.description}</div>
+          </Link>
+        ))}
+      </div>
+    </div>
+  );
+}
+
+export default Sponsors;
