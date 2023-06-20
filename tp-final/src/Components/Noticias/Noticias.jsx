0f5603c6722f68c84e61a9cc3fab4a31fce3ca03
Melina Figueroa <melinafigueroa@tdf.edu.ar>
2023-06-19 22:15:58 -0300

    se agregan categorias a la seccion noticias, quedan (funcional), a evaluar cambios futuros

--- tp-final/src/Components/Noticias/Noticias.jsx
+++ tp-final/src/Components/Noticias/Noticias.jsx
@@ -1,10 +1,62 @@
-import Content from "../Content/Content"
+import React, { useState } from 'react';
+import './noticias.css';
 
 const Noticias = () => {
-    return(
-    
-    <Content/>
-    )
-}
+  const [categoria, setCategoria] = useState('todas'); // Estado para almacenar la categoría seleccionada //
 
-export default Noticias
+  const noticias = [
+    {categoria: 'juveniles' , titulo: 'Noticia 1' },
+    { titulo: 'Noticia 2', categoria: 'infantiles' },
+    { titulo: 'Noticia 3', categoria: 'femeninos' },
+    { titulo: 'Noticia 4', categoria: 'adultos' },
+    { titulo: 'Noticia 5', categoria: 'categoria1' },
+    { titulo: 'Noticia 6', categoria: 'categoria3' },
+    // ...
+  ];
+
+  const categorias = ['todas', 'categoria1', 'categoria2', 'categoria3'];
+
+  // Función para filtrar las noticias según la categoría seleccionada //
+  const filtrarNoticias = (noticias, categoria) => {
+    if (categoria === 'todas') {
+      return noticias;
+    } else {
+      return noticias.filter(noticia => noticia.categoria === categoria);
+    }
+  };
+
+  const noticiasFiltradas = filtrarNoticias(noticias, categoria);
+
+  return (
+    <div className="noticias-container">
+      <h1 className="titulo">Noticias</h1>
+      <div className="categorias">
+        <button
+          className={`categoria ${categoria === 'todas' ? 'categoria-activa' : ''}`}
+          onClick={() => setCategoria('todas')}
+        >
+          Todas
+        </button>
+        {categorias.map(cat => (
+          <button
+            key={cat}
+            className={`categoria ${categoria === cat ? 'categoria-activa' : ''}`}
+            onClick={() => setCategoria(cat)}
+          >
+            {cat}
+          </button>
+        ))}
+      </div>
+      <div className="noticias-grid">
+        {noticiasFiltradas.map(noticia => (
+          <div key={noticia.titulo} className="noticias-item">
+            <h2 className="noticias-titulo">{noticia.titulo}</h2>
+            <p className="noticias-categoria">Categoría: {noticia.categoria}</p>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default Noticias;
