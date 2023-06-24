2895d806eea2e26bbcd87a70d2790216f1991c84
Melina Figueroa <melinafigueroa@tdf.edu.ar>
2023-06-23 23:31:27 -0300

    se modifica color en footer

--- tp-final/src/Components/Footer/Footer.jsx
+++ tp-final/src/Components/Footer/Footer.jsx
@@ -1,46 +1,70 @@
-import './Footer.css'
-import  logoCLA  from "../../Imgenes/LogoCLA2.png";
-import { FaFacebook, FaInstagram, FaYoutube } from "react-icons/fa";
+import React from 'react';
+import { FaFacebook, FaInstagram, FaYoutube } from 'react-icons/fa';
 import { Link } from 'react-router-dom';
+import { Container, Row, Col } from 'react-bootstrap';
+import './Footer.css';
+import logoCLA from '../../Imgenes/imagenes-Header-Footer/logoCla.svg';
 
-function Footer() {
-    return (
-        <footer className="footer">
-
-            <div className="div-footer">
-
-                <div className='div-logo-footer'>
+const Footer = () => {
+  return (
+    <footer className="footer">
+      <Container>
+        <Row className="align-items-center">
+             <Col xs={12} sm={4}>
+             <div className='logo-titulo'>
+            <div className='div-logo'>
                 <Link to="/">
-                    <img className='logoFooter' href={"/inicio"} src={logoCLA} alt="Logo"/>  
-                </Link>   
-                </div>
-                <div className='div-eslogan'>
-                     <div>Club familiar de Rugby y Hockey más austral del mundo.</div>
-                </div>
-
-                <div className='div-redes'>
-                    <div className='div-redes-club'>Nuestro club en redes</div>
-
-                    <div className="div-icons">
-                        <div><a target="_blank" rel="noreferrer" href="https://www.instagram.com/clublasaguilas/" title="Instagram">
-                            <FaInstagram color='black' /></a></div>
-                        <div><a target="_blank" rel="noreferrer" href="https://www.facebook.com/clublasaguilasushuaia/?locale=es_LA" title="Facebook">
-                            <FaFacebook color='black' /></a></div>
-                        <div><a target="_blank" rel="noreferrer" href="https://www.youtube.com/@clublasaguilas-analisisdev8566" title="Youtube">
-                            <FaYoutube color='black'/></a></div>
-                    </div>
-                </div>
+                    <img className='logo' href={"/"} src={logoCLA} alt="Logo"/>  
+                </Link>        
             </div>
-            <div className='sub-footer'>
-            <div className="div-copyright">
-                <p>&copy; 2023 - MentesDeMentes</p>
-                </div>
-                <div className='div-contacto'>
-                    <Link className='link-contacto' to="/contacto">Contáctanos</Link> 
-                 </div>
+            </div>        
+            </Col>
+            <Col xs={12} sm={4}>
+            <p className='titulo'>El club familiar de Rugby y Hockey más austral del mundo</p>
+          </Col>
+          <Col xs={12} sm={4} className="text-center text-sm-end">
+            <div className="div-redes">
+              <div className="titulo-redes">Nuestro club en redes</div>
+              <div className="redes">
+                <a
+                  target="_blank"
+                  rel="noreferrer"
+                  href="https://www.instagram.com/clublasaguilas/"
+                  title="Instagram"
+                >
+                  <FaInstagram color="black" size={24} />
+                </a>
+                <a
+                  target="_blank"
+                  rel="noreferrer"
+                  href="https://www.facebook.com/clublasaguilasushuaia/?locale=es_LA"
+                  title="Facebook"
+                >
+                  <FaFacebook color="black" size={24} />
+                </a>
+                <a
+                  target="_blank"
+                  rel="noreferrer"
+                  href="https://www.youtube.com/@clublasaguilas-analisisdev8566"
+                  title="Youtube"
+                >
+                  <FaYoutube color="black" size={24} />
+                </a>
+              </div>
             </div>
-        </footer>
-    );
-}
+            <div className="text-center text-sm-end">
+              <Link className="link-contacto" to="/contacto">
+                Contáctanos
+              </Link>
+            </div>
+          </Col>
+          <Col xs={12} className="text-center">
+            <p>&copy; 2023 - MentesDeMentes</p>
+          </Col> 
+          </Row>
+      </Container>
+    </footer>
+  );
+};
 
-export default Footer
+export default Footer;
