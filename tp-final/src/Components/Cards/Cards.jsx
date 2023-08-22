5715fc1e1c210b45db6d7b62faa930d6eb3396bd
Emanuel <emanuellucietti@tdf.edu.ar>
2023-08-22 08:07:17 -0300

    se trato de acomodar las card del home

--- tp-final/src/Components/Cards/Cards.jsx
+++ tp-final/src/Components/Cards/Cards.jsx
@@ -6,65 +6,89 @@ import Image2 from '../../Imagenes/imgNoticias/2.svg';
 import Image3 from '../../Imagenes/imgNoticias/3.svg';
 import sidebar1 from '../../Imagenes/imgNoticias/sidebar1.svg';
 import './cards.css';
-
-const cardData = [
-  {
-    image: Image1,
-    title: 'Águilas Jr',
-    descripcion: 'Nuestra escuela de rugby para niños ofrece una experiencia deportiva divertida y educativa.',
-  },
-  {
-    image: Image2,
-    title: 'Hockey Femenino',
-    descripcion: 'El equipo de hockey femenino del club ha tenido una destacada participación...',
-  },
-  {
-    image: Image3,
-    title: 'Noticia 3',
-    descripcion: 'Contenido de la noticia 3.',
-  },
-  {
-    image: Image3,
-    title: 'Noticia 4',
-    descripcion: 'Contenido de la noticia 4.',
-  },
-];
-
 const Cards = () => {
+  const cardData = [
+    {
+      image: Image1,
+      title: 'Águilas Jr',
+      text: 'Nuestra escuela de rugby para niños ofrece una experiencia deportiva divertida y educativa.',
+    },
+    {
+      image: Image2,
+      title: 'Hockey Femenino',
+      text: 'El equipo de hockey femenino del club ha tenido una destacada participación...',
+    },
+    {
+      image: Image3,
+      title: 'Noticia 3',
+      text: 'Contenido de la noticia 3.',
+    },
+    {
+      image: Image3,
+      title: 'Noticia 4',
+      text: 'Contenido de la noticia 4.',
+    },
+  ];
+
   return (
-    <Container fluid className='contenedor'>
-      <Row>
-        {cardData.map((card, index) => (
-          <Col md='6' key={index}>
-            <Card className='card'>
-              <Card.Body className='cardBody'>
-                <Card.Title className='cardTitle'>{card.title}</Card.Title>
-                <Card.Subtitle className='cardSubtitle'>{card.descripcion}</Card.Subtitle>
-                <Card.Img className='cardImg' variant='top' src={card.image} />
+    <Container fluid>
+      <Row className='row-contenedor'> 
+      <div className='padre'>   
+        <div className='hijoUno'>
+          <Col>        
+          {cardData.slice(0, 2).map((card, index) => (
+
+            <Card key={index}>
+              <Card.Body>
+                <Card.Img variant="top" src={card.image} className="img-fluid" />
+                <Card.Title>{card.title}</Card.Title>
                 <Card.Text>{card.text}</Card.Text>
                 <Button variant="dark" className='colorBoton'>Leer más...</Button>
               </Card.Body>
             </Card>
-          </Col>
-        ))}
+          ))}
+        </Col>   
+        </div>
+
+        <div className='hijoDos'>
         <Col>
-          <Card className='sideBar'>
-            <Card.Body>
-              <Card.Img variant="top" src={sidebar1} className="img-fluid imgSideBar" />
-              <Card.Text className='cardText'>
-                Contenido del sidebar.
-                mas contenidohhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhh
-                jjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjj
-                jjjjjjjjjjjjjjjjjjjjjjjjjjjjj
-              </Card.Text>
-              <Button variant="dark" className='colorBoton'>Leer más...</Button>
-            </Card.Body>
-          </Card>
-          <VideoPlayer />
-           </Col>
+          {cardData.slice(2, 4).map((card, index) => (
+            <Card key={index}>
+              <Card.Body>
+                <Card.Img variant="top" src={card.image} className="img-fluid" />
+                <Card.Title>{card.title}</Card.Title>
+                <Card.Text>{card.text}</Card.Text>
+                <Button variant="dark" className='colorBoton'>Leer más...</Button>
+              </Card.Body>
+            </Card>
+          ))}
+        </Col>
+        </div>
+       <div className='hijoTres'>
+       <Col>
+          <aside>
+            <Card className='card-aside'>
+              <Card.Body className='card-body-aside'>
+                <Card.Img variant="top" src={sidebar1} style={{ height: '30rem' }} className="img-fluid" />
+                <Card.Text>Contenido del sidebar.</Card.Text>
+                <Button variant="dark" className='colorBoton'>Leer más...</Button>
+              </Card.Body>
+            </Card>
+           
+            {/* VideoPlayer */}
+            <VideoPlayer />
+            
+          </aside>
+        </Col>
+       
+       </div>
+      </div>
       </Row>
     </Container>
   );
 }
 
 export default Cards;
+
+
+
