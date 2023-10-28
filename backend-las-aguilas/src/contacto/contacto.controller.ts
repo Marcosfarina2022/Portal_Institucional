446d3be8244d590b27ce770adb06231ce9306d58
Diaz Fernando <fernandodiaz@tdf.edu.ar>
2023-10-28 16:46:01 -0300

    se realizo el endpoint de contacto

--- backend-las-aguilas/src/contacto/contacto.controller.ts
+++ backend-las-aguilas/src/contacto/contacto.controller.ts
@@ -6,11 +6,11 @@ import { ContactoDto } from './dto/contacto.dto';
 export class ContactoController {
   constructor(private readonly contactoService: ContactoService) {}
 
-  @Post()
-  create(@Body() createContactoDto: ContactoDto) {
-    return this.contactoService.create(createContactoDto);
+  @Post('agregar')
+  async create(@Body() contactoDto: ContactoDto):Promise<ContactoDto> {
+    return await this.contactoService.create(contactoDto);
   }
-
+ 
   @Get()
   findAll() {
     return this.contactoService.findAll();
--- backend-las-aguilas/src/contacto/contacto.service.ts
+++ backend-las-aguilas/src/contacto/contacto.service.ts
@@ -1,10 +1,31 @@
-import { Injectable } from '@nestjs/common';
+import { HttpException, HttpStatus, Injectable } from '@nestjs/common';
 import { ContactoDto } from './dto/contacto.dto';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Contacto } from './entities/contacto.entity';
+import { Repository } from 'typeorm';
 
 @Injectable()
 export class ContactoService {
-  create(createContactoDto: ContactoDto) {
-    return 'This action adds a new contacto';
+
+  constructor(@InjectRepository(Contacto)
+              private contactoRepository:Repository<Contacto>){} 
+            
+              
+
+  async create(contactoDto: ContactoDto):Promise<ContactoDto>{ 
+    try{
+      let contacto:Contacto = await this.contactoRepository.save(new Contacto(contactoDto.consulta,contactoDto.nombre,contactoDto.apellido,contactoDto.correo_electronico));
+      if(contacto)
+         return contactoDto;
+     else
+         throw new Error('No se pudo crear la consulta');
+  }
+  catch(error){
+      throw new HttpException({
+          status: HttpStatus.NOT_FOUND,
+          error: 'Error en contacto - ' + error
+      },HttpStatus.NOT_FOUND)
+  }
   }
 
   findAll() {
