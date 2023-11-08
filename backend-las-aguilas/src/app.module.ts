823d1505195dd99fdd2d3a889f05c39b856ac436
Marcosfarina2022 <marcosfarina@tdf.edu.ar>
2023-11-07 21:10:40 -0300

    Se actualiza el front

--- backend-las-aguilas/src/app.module.ts
+++ backend-las-aguilas/src/app.module.ts
@@ -20,7 +20,7 @@ import { ComentariosGaleriaModule } from './comentarios_galeria/comentarios_gale
       host: 'localhost',
       port: 3306,
       username: 'root',
-      password: 'tino3719',
+      password: 'marcos_2326',
       database: 'db_aguilas',
       entities: ["dist/**/**.entity{.ts,.js}"],
       synchronize: true,
--- tp-final/src/Components/Noticias/Noticias.jsx
+++ tp-final/src/Components/Noticias/Noticias.jsx
@@ -1,60 +1,3 @@
-<<<<<<< HEAD
-import React, { useState } from 'react';
-import { Container, Row, Col, Card } from 'react-bootstrap';
-import { useSpring, animated } from 'react-spring';
-import { Link } from 'react-router-dom';
-import NavDropdown from 'react-bootstrap/NavDropdown';
-
-import noticia1 from '../../Imagenes/imgNoticias/1.svg';
-import noticia2 from '../../Imagenes/imgNoticias/2.svg';
-import noticia3 from '../../Imagenes/imgNoticias/3.svg';
-import noticia4 from '../../Imagenes/imgNoticias/4.jpeg';
-import noticia5 from '../../Imagenes/imgNoticias/5.jpeg';
-import noticia6 from '../../Imagenes/imgNoticias/6.jpeg';
-
-const noticiasData = [
-  {
-    nombre: 'Noticia 1',
-    categoria: 'Categoria1',
-    descripcion: 'Descripción de la noticia 1',
-    foto: noticia1,
-    linkNoticia: '/noticia/Rugby-Juveniles',
-    
-  },
-  {
-    nombre: 'Noticia 2',
-    categoria: 'Categoria2',
-    descripcion: 'Descripción de la noticia 2',
-    foto: noticia1,
-    linkNoticia: '/noticia/2',
-    
-  },
-  {
-    nombre: 'Noticia 3',
-    categoria: 'Categoria 3',
-    descripcion: 'Descripción de la noticia 3',
-    foto: noticia1,
-    linkNoticia: '/noticia/3',
-    
-  },
-  // Agrega más noticias aquí
-];
-
-const CategoriasDropdown = ({ categoriaSeleccionada, setCategoriaSeleccionada }) => (
-  <NavDropdown
-    title={`Categoría: ${categoriaSeleccionada}`}
-    onSelect={(eventKey) => setCategoriaSeleccionada(eventKey)}
-  >
-    <NavDropdown.Item eventKey='Rugby-Juveniles'>Rugby-Juveniles</NavDropdown.Item>
-    <NavDropdown.Item eventKey='Rugby-Adultos'>Rugby-Adultos</NavDropdown.Item>
-    <NavDropdown.Item eventKey='Hockey-Juveniles'>Hockey-Juveniles</NavDropdown.Item>
-    {/* Agrega más categorías según tus necesidades */}
-  </NavDropdown>
-);
-
-const NoticiaCard = ({ noticia }) => {
-  const fadeIn = useSpring({
-=======
 import { Container, Row, Col, Card } from 'react-bootstrap';
 //import { useSpring, animated } from 'react-spring';
 import { Link } from 'react-router-dom';
@@ -109,7 +52,6 @@ const noticias = [
 
 const Noticias = () => {
   /*const fadeIn = useSpring({
->>>>>>> origin/testing-backend
     from: { opacity: 0 },
     to: { opacity: 1 },
     config: { duration: 500 },
@@ -120,46 +62,6 @@ const Noticias = () => {
     config: { duration: 500 }, // Ajusta la duración de la animación
   });
 
-<<<<<<< HEAD
-  return (
-    <Col xs={12} md={4} className='mt-4 mb-4'>
-      <Card className='cardMiembros'>
-        <Card.Body className='cardBody'>
-          <animated.div style={fadeIn}>
-            <Card.Title className='cardTitle'>{noticia.nombre}</Card.Title>
-            <Card.Subtitle className='cardSubtitle'>
-              <p className='text-start'>{noticia.descripcion}</p>
-            </Card.Subtitle>
-            <Card.Img className='cardImg' variant='top' src={noticia.foto} />
-            <Link
-              to={noticia.linkNoticia}
-              target='_blank'
-              rel='noopener noreferrer'
-              className='cardLink'
-            >
-              Ver más
-            </Link>
-          </animated.div>
-        </Card.Body>
-      </Card>
-    </Col>
-  );
-};
-
-const Noticias = () => {
-  const [categoriaSeleccionada, setCategoriaSeleccionada] = useState('Todas');
-
-  return (
-    <Container className='containerSobreNosotros text-center'>
-      <Row>
-        <h1>Últimas noticias</h1>
-      </Row>
-      <Row>
-        {noticiasData.map((noticia, index) => (
-          (categoriaSeleccionada === 'Todas' || noticia.categoria === categoriaSeleccionada) ? (
-            <NoticiaCard key={index} noticia={noticia} />
-          ) : null
-=======
   return (
     <Container className='containerNoticias text-center fluid'>
       <Row>
@@ -182,7 +84,6 @@ const Noticias = () => {
               </Card.Body>
             </Card>
           </Col>
->>>>>>> origin/testing-backend
         ))}
         <CategoriasDropdown
           categoriaSeleccionada={categoriaSeleccionada}
