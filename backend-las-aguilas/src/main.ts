fb430d3cae4911f1f09ffb95138cbe6e347ce54f
Melina-figueroa <melinafigueroa@tdf.edu.ar>
2023-11-09 21:33:36 -0300

    cors

--- backend-las-aguilas/src/main.ts
+++ backend-las-aguilas/src/main.ts
@@ -10,7 +10,7 @@ async function bootstrap() {
 
   // Configura CORS
   const corsOptions: CorsOptions = {
-    origin: 'http://localhost:3000', 
+    origin: ['http://localhost:3000','https://portal-institucional-5edb1.web.app'], 
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
     credentials: true,
   };
