026a81ebcebc52ceb710e84d611e5ce916a08b1e
Emanuel <emanuellucietti@tdf.edu.ar>
2023-12-05 19:58:01 -0300

    se actualiza back-end

--- tp-final/src/Components/NavBar/NavBar.jsx
+++ tp-final/src/Components/NavBar/NavBar.jsx
@@ -1,18 +1,9 @@
-<<<<<<< HEAD
-import React from "react";
-import { Container, Nav, Navbar, NavDropdown } from "react-bootstrap";
-import { LinkContainer } from "react-router-bootstrap";
-import RugbyAdultos from "../Noticias/RugbyAdultos";
-import RugbyJuveniles from "../Noticias/RugbyJuveniles";
-
-=======
 import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 import NavDropdown from 'react-bootstrap/NavDropdown';
 import { LinkContainer } from 'react-router-bootstrap'
 import './NavBar.css'
->>>>>>> origin/testing-backend
 
 
 const NavBar = () => {
@@ -28,22 +19,6 @@ const NavBar = () => {
                 Inicio
               </Nav.Link>
             </LinkContainer>
-<<<<<<< HEAD
-              <NavDropdown title="Noticias" id="basic-nav-dropdown" className="color">
-                <LinkContainer to="/noticias/rugby-juveniles">
-                  <NavDropdown.Item>Rugby-Juveniles</NavDropdown.Item>
-                </LinkContainer>
-                <LinkContainer to="/noticias/rugby-adultos">
-                <NavDropdown.Item >Rugby-Adultos</NavDropdown.Item>
-                </LinkContainer>
-                <LinkContainer to="/noticias/hockey-juveniles">
-                  <NavDropdown.Item>Hockey-Juveniles</NavDropdown.Item>
-                </LinkContainer>
-                <LinkContainer to="/noticias/hockey-adultos">
-                  <NavDropdown.Item>Hockey-Adultos</NavDropdown.Item>
-                </LinkContainer>
-              </NavDropdown>
-=======
             <NavDropdown title="Noticias" id="basic-nav-dropdown" className="color">
               <LinkContainer to="/noticias/rugby-juveniles">
                 <NavDropdown.Item>Rugby- Juveniles</NavDropdown.Item>
@@ -59,7 +34,6 @@ const NavBar = () => {
                 <NavDropdown.Item>Hockey-Adultos</NavDropdown.Item>
               </LinkContainer>
             </NavDropdown>
->>>>>>> origin/testing-backend
             <LinkContainer to="/galeria">
               <Nav.Link href="#galeria" className="color rounded">
                 Galeria
--- tp-final/src/Components/Noticias/Noticias.jsx
+++ tp-final/src/Components/Noticias/Noticias.jsx
@@ -1,79 +1,3 @@
-<<<<<<< HEAD
-import React, { useState } from "react";
-import { Container, Row, Col, Card } from "react-bootstrap";
-import { useSpring, animated } from "react-spring";
-import { Link } from "react-router-dom";
-import RugbyAdultos from "./RugbyAdultos";
-import RugbyJuveniles from "./RugbyJuveniles";
-import HockeyAdultos from "./HockeyAdultos";
-import HockeyJuveniles from "./HockeyJuveniles";
-import "./noticias.css";
-
-const Noticias = () => {
-  const [categoriaSeleccionada, setCategoriaSeleccionada] = useState(null);
-  const fadeIn = useSpring({
-    from: { opacity: 0 },
-    to: { opacity: 1 },
-    config: { duration: 500 },
-  });
-
-  let noticiasFiltradas;
-
-  if (categoriaSeleccionada === "rugby-adultos") {
-    noticiasFiltradas = RugbyAdultos;
-  } else if (categoriaSeleccionada === "rugby-juveniles") {
-    noticiasFiltradas = RugbyJuveniles;
-  } else if (categoriaSeleccionada === "hockey-adultos") {
-    noticiasFiltradas = HockeyAdultos;
-  } else if (categoriaSeleccionada === "hockey-juveniles") {
-    noticiasFiltradas = HockeyJuveniles;
-  } else {
-    // Si no se selecciona una categoría, se muestran todas las noticias
-    noticiasFiltradas = [
-      ...RugbyAdultos,
-      ...HockeyJuveniles,
-      ...HockeyAdultos,
-      ...RugbyJuveniles,
-    ];
-  }
-
-  return (
-    <Container className="text-center">
-      <Row>
-        <h1>Te damos la Bienvenida a la sección noticias</h1>
-        <Col className="text-center">
-          <h5>Aquí podrás encontrar la última información del club</h5>
-        </Col>
-      </Row>
-      <Row>
-        {noticiasFiltradas.map((noticia, index) => (
-          <Col key={index} xs={12} md={4} className="mt-4 mb-4">
-            <Card className="cardMiembros">
-              <Card.Body className="cardBody">
-                <animated.div style={fadeIn}>
-                  <Card.Title className="cardTitle">
-                    {noticia.titulo_noticia}
-                  </Card.Title>
-                  <Card.Subtitle className="cardSubtitle">
-                    <ul className="text-start">
-                      {noticia.descripcion.map((descripcion, i) => (
-                        <li key={i}>{descripcion}</li>
-                      ))}
-                    </ul>
-                  </Card.Subtitle>
-                  <Card.Img
-                    className="cardImg"
-                    variant="top"
-                    src={noticia.foto}
-                  />
-                  <Link
-                    to={noticia.linkNoticia}
-                    target="_blank"
-                    rel="noopener noreferrer"
-                    className="cardLink"
-                  >
-                    Ver más
-=======
 import { Container, Row, Col, Card } from 'react-bootstrap';
 //import { useSpring, animated } from 'react-spring';
 import { Link } from 'react-router-dom';
@@ -172,7 +96,6 @@ const Noticias = (props) => {
                   </Card.Subtitle>
                   <Link  to={noticia.linkNoticia}target='_blank'rel='noopener noreferrer'className='cardLink'>
                   <Card.Img className='cardImgNoticias' variant='top' src={noticia.foto} />
->>>>>>> origin/testing-backend
                   </Link>
                 </animated.div>
               </Card.Body>
@@ -184,8 +107,4 @@ const Noticias = (props) => {
   );
 };
 
-<<<<<<< HEAD
 export default Noticias;
-=======
-export default Noticias;
->>>>>>> origin/testing-backend
